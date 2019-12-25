75ecf75e24b54327bf61a7b137e05c54467d826e
vuongduong12345 <vuongtatduong@gmail.com>
2019-12-25 13:09:48 +0700

    em tạo ra 1 component Test2 và in ra thẻ h1 với nội dung là giá trị của data.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test2 title="day la dung drops"/>
+    <Test2 demo="data1234"/>
+    <Test2 demo2="data5678"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test2 from './components/test2'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Test2
+      }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>lay data tu props</h1>
+        <h1>{{title}}</h1>
+        <h1>{{demo}}</h1>
+        <h1>{{demo2}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "haha",
+    props: {
+        title: String
+    },
+    data() {
+        return {
+            demo: "xin chao",
+            demo2: "xin chao 2"
+        }
+    }
+}
+</script>
+<style>
+
+</style>
